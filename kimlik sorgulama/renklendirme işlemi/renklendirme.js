f7e8929df5b00ff9382df8647630a5f5e59c3351
Yağmur Genç <yagmurgenc58@gmail.com>
2024-07-31 13:09:54 +0300

    havadurumu

--- kimlik sorgulama/renklendirme işlemi/renklendirme.js
+++ kimlik sorgulama/renklendirme işlemi/renklendirme.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-    // Renk seçici için olay dinleyicisi
+   
     $('#colorPicker').on('input', function() {
         var selectedColor = $(this).val();
         $('body').css('background-color', selectedColor);
     });
 
-    // Rastgele renk seçici için olay dinleyicisi
+   
     $('#randomColorButton').on('click', function() {
         var randomColor = getRandomColor();
         $('body').css('background-color', randomColor);
     });
 
-    // Rastgele renk oluşturma fonksiyonu
+   
     function getRandomColor() {
         var letters = '0123456789ABCDEF';
         var color = '#';
